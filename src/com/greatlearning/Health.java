e5279b1e08b1e8f171faec9f2b9f7da89d7d2a80
KFaisalRaza <148312120+KFaisalRaza@users.noreply.github.com>
2023-11-12 11:28:56 +0530

    Coding challenge2 solution

---
+++ src/com/greatlearning/Health.java
@@ -0,0 +1,7 @@
+package com.greatlearning;
+
+public class Health {
+protected int age = 25;
+protected float weight = 60f;
+protected float bmi = 20.9f;
+}
---
+++ src/com/greatlearning/HealthDriver.java
@@ -0,0 +1,13 @@
+package com.greatlearning;
+
+public class HealthDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Health person = new Health();
+		System.out.println(person.age);
+		System.out.println(person.weight);
+		System.out.println(person.bmi);
+	}
+
+}
